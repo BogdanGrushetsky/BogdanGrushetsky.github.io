582c5b3420ee088e40822af4984ff2f82e563ae4
Bogdan Grushetsky <bogdan1grushetsky@gmail.com>
2021-12-02 14:28:58 +0200

    test version 2.0

--- js/script.js
+++ js/script.js
@@ -1,28 +1,50 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
+  hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
+  });
 
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
+  closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
 
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+  const counters = document.querySelectorAll(".skills__ratings-counter"),
+    lines = document.querySelectorAll(".skills__ratings-line span");
 
-counters.forEach( (item, i) => {
+  counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
-});
+  });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
+
+  const boxes = document.querySelectorAll(".box");
+
+  window.addEventListener("scroll", checkBoxes);
+
+  checkBoxes();
+
+  function checkBoxes() {
+    const triggerBottom = (window.innerHeight / 5) * 4;
+
+    boxes.forEach(box => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.classList.add("show");
+      } else {
+        box.classList.remove("show");
+      }
+    });
+  }
 });
